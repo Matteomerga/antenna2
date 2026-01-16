6079b0be23d2bdeaca25ba249b5d3e8ab53ed167
fouadr127 <129856288+fouadr127@users.noreply.github.com>
2026-01-16 15:17:03 +0100

    Updated version
    Better look and (maybe) modified for older versions

--- analisi dati offline/GreenMECC.m
+++ analisi dati offline/GreenMECC.m
@@ -3,36 +3,46 @@ M = mylaps(Data);
 
 Lap = cell2mat(M(1));
 
-LapData = figure(Name="Lap Data", NumberTitle="off");
-plot(Lap(:,1), Lap(:,2));
-LapNumber = uidropdown(LapData, ...
+LapData = uifigure(Name="Lap Data", NumberTitle="off");
+g = uigridlayout(LapData);
+g.RowHeight = {'0.5x', 22, 22, 22, '3x'};
+g.ColumnWidth = {100,'1x'};
+ax = uiaxes(g);
+ax.Layout.Row = [2 5];
+ax.Layout.Column = 2;
+plot(ax, Lap(:,1), Lap(:,2));
+LapNumber = uidropdown(g, ...
     Items=["Lap1", "Lap2", "Lap3", "Lap4", "Lap5", "Lap6", "Lap7", "Lap8", "Lap9", "Lap10", "Lap11"], ...
-    ItemsData=[1 2 3 4 5 6 7 8 9 10 11], ...
-    Position=[10 500 100 22]);
-DataType = uidropdown(LapData, ...
+    ItemsData=[1 2 3 4 5 6 7 8 9 10 11]);
+DataType = uidropdown(g, ...
     Items=["Voltage", "Current", "Speed", "Position"], ...
-    ItemsData=[2 3 4 5], ...
-    Position=[10 470 100 22]);
-Compare = uidropdown(LapData, ...
+    ItemsData=[2 3 4 5]);
+Compare = uidropdown(g, ...
     Items=["None", "Voltage", "Current", "Speed"], ...
     ItemsData=[0 2 3 4], ...
-    ValueChangedFcn=@(src,event) updatePlot(LapNumber, M, DataType, src), ...
-    Position=[10 440 100 22]);
-DataType.ValueChangedFcn=@(src,event) updatePlot(LapNumber, M, src, Compare);
-LapNumber.ValueChangedFcn=@(src,event) updatePlot(src, M, DataType, Compare);
+    ValueChangedFcn=@(src,event) updatePlot(LapNumber, M, DataType, src, ax));
+DataType.ValueChangedFcn=@(src,event) updatePlot(LapNumber, M, src, Compare, ax);
+LapNumber.ValueChangedFcn=@(src,event) updatePlot(src, M, DataType, Compare, ax);
+%Poistioning of drop downs
+LapNumber.Layout.Row = 2;
+LapNumber.Layout.Column = 1;
+DataType.Layout.Row = 3;
+DataType.Layout.Column = 1;
+Compare.Layout.Row = 4;
+Compare.Layout.Column = 1;
 
-function updatePlot(src, M, src2, src3)
+function updatePlot(src, M, src2, src3, ax)
     val = src.Value;
     D = src2.Value;
     C = src3.Value;
     data = cell2mat(M(val));
     if D == 5
-        plot(data(:,5),data(:,6));
+        plot(ax, data(:,5),data(:,6));
         return
     end
     if C~=0
-        plot(data(:,1),data(:,D),data(:,1),data(:,C));
+        plot(ax, data(:,1),data(:,D),data(:,1),data(:,C));
         return
     end
-    plot(data(:,1),data(:,D));
+    plot(ax, data(:,1),data(:,D));
 end
