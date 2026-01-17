df6baa55575d56261357a8b71bb012de91878641
Matteomerga <ma.merga04@gmail.com>
2026-01-17 16:01:27 +0100

    aggiunto energia e potenza
    Added energy and power calculations to lap data extraction.

--- analisi dati offline/mylaps.m
+++ analisi dati offline/mylaps.m
@@ -1,28 +1,41 @@
-function[M] = mylaps(Data)
+function [M] = mylaps(Data)
     M = cell(1,11);
-    l = 1; % M index
+    l = 1;
     prev = 0;
-    startl=0;
-    endl=0;
-    for t = 1:length( Data(:,1) )
-        curr = Data(t,4);
+    startl = 0;
+    endl = 0;
+    th = 0.05;   % soglia (float)
     
-        if prev==0 && curr > 0
+    for t = 1:length(Data(:,1))
+        speed = Data(t,4);
+
+        if prev == 0 && speed > 0
             startl = t;    
         end
-    
-        if prev > 0 && curr == 0
+
+        if prev > 0 && speed == 0
             endl = t;
         end
-        
+
         if not(startl == 0) && not(endl == 0)
-            M{l} = Data([startl: endl], :);
-            l = l+1;
+            lapData = Data(startl:endl, :);
+
+            %energia
+            V = lapData(:,2);
+            I = lapData(:,3); 
+            tLap = lapData(:,1);
+
+            P = V .* I; 
+            E = cumtrapz(tLap, P);
+            lapData = [lapData E P];
+
+            M{l} = lapData;
+            l = l + 1;
             startl = 0; 
             endl = 0;
             %fprintf('lap %d completato\n',l);
         end  
-        
-        prev = curr;
+
+        prev = speed;
     end
 end
