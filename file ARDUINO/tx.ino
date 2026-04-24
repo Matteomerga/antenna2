419d497a0d6ce7f710d8ac5c75facd568bb362a8
federico23-poli <federico.lai@mail.polimi.it>
2026-04-24 22:06:48 +0200

    Update tx.ino

--- file ARDUINO/tx.ino
+++ file ARDUINO/tx.ino
@@ -1,4 +1,4 @@
-//TRASMETTITORE
+// TRASMETTITORE
 #include <SPI.h>
 #include "printf.h"
 #include <RF24.h>
@@ -8,89 +8,72 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
 
-int block=0;
+int block = 0;
 
 // Pin definitions
-#define CE_PIN       8  // pin NRF24
-#define CSN_PIN      9 
-#define SD_CS_PIN    10  // Pin for SD module
+#define CE_PIN       8
+#define CSN_PIN      9
+#define SD_CS_PIN    10
 #define ENCODER_PIN  3
 #define TX_PIN       7
 #define RX_PIN       6
+
 const int voltPin = A2;
 const int currPin = A1;
-const uint8_t curr_motorPin = A3;  //ultima aggiunta: corrente misurata al motore
-//const int NUM_SAMPLES = 10;
+const uint8_t curr_motorPin = A3;
 
+const int pacchetti_al_secondo = 2;
+const unsigned long delta = 1000000 / pacchetti_al_secondo;
 
-const int pacchetti_al_secondo = 10;
-const unsigned long delta = 1000000/pacchetti_al_secondo;
-
-float kv = 0.0606203;
-float ki = - 0.073982;
-float v_offset = 1.4425;
-double zeroCurr = 0;
-float sumCurrent = 0;
-float sumVoltage = 0;
-float sumCurrentMotor = 0;
-float motorCurrentRaw = 0;
+int v_offset = 1;
+long int zeroCurr = 0;
+long int zeroCurrMotor = 0;
+long int sumCurrent_raw = 0;
+long int sumVoltage_raw = 0;
+long int sumCurrentMotor_raw = 0;
 int ncampioni = 0;
-const float CurrMotorConst     = 185.0; // Costante di conversione
-float CurrMotorOffset_voltage    = 2.65;
-// //float samplesMot[NUM_SAMPLES];        // buffer valori
-// int indexMot = 0;                    // indice corrente nel buffer
-// float totalMot = 0.0;                   // somma dei valori
-// float SenseAverageMot = 0.0;
-// bool CalibrationTimeEnded = 0;
-bool firstrun = 1;
 
-
-char dataStr[80];  
-char vStr[10];
-char cStr[10];
-char cmStr[10];
-char velStr[10];
-
+char dataStr[80];
+bool headerScritto = false;
 
 // Radio setup
 RF24 radio(CE_PIN, CSN_PIN);
 uint8_t address[][6] = { "1Node", "2Node" };
 bool radioNumber = 1;
 
-//Gps setup
+// GPS setup
 SoftwareSerial gpsSerial(TX_PIN, RX_PIN); // RX, TX
 TinyGPSPlus gps;
 
-// dati per calcolo velocita
+// Dati per calcolo velocità
 const int n_fori = 60;
 float circonferenza = 2 * PI * 0.265;
-volatile unsigned long lastMicrov = 0;
-volatile unsigned long deltatv = 0;
-volatile unsigned long currentMicrov = 0;
-volatile unsigned long lastMicrovm = 0;
-volatile unsigned long deltatvm = 0;
-volatile float velocitaCalcolata = 0;
-volatile float velocitaCalcolatam = 0;
+// volatile unsigned long lastMicrov = 0;
+// volatile unsigned long deltatv = 0;
+// volatile unsigned long currentMicrov = 0;
+// volatile unsigned long lastMicrovm = 0;
+// volatile unsigned long deltatvm = 0;
+// volatile float velocitaCalcolata = 0;
+// volatile float velocitaCalcolatam = 0;
+volatile int tick = 0;
 const unsigned long tempoMaxFermo = 1000000UL;
-int j=0;
-int n_media=10;
+int j = 0;
+int n_media = 10;
 
 int i = 0;
 unsigned long currentMicros = 0;
 unsigned long previousMicros = 0;
 
-
-
 int fileNumber = 1;
 char fileName[15];
 
-
-//stuct dei dati da inviare
+// Struct dei dati da inviare
 struct Mystruct {
-  float velocita;
-  float voltage;
-  float current;
-  float currentMotor;
+  int velocita_gps;
+  int voltage_raw;
+  int current_raw;
+  int currentMotor_raw;
+  int tick_count;
   unsigned long int lat;
   unsigned long int lng;
   unsigned long int micro;
@@ -100,58 +83,46 @@ struct Mystruct {
 Mystruct payload;
 File dataFile; // SD file
 
-
 void setup() {
+  // Pin setup
   pinMode(currPin, INPUT);
   pinMode(voltPin, INPUT);
   pinMode(curr_motorPin, INPUT);
   pinMode(SD_CS_PIN, OUTPUT);
   pinMode(CSN_PIN, OUTPUT);
   pinMode(ENCODER_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), calcolaFrequenza, RISING);
-  digitalWrite(SD_CS_PIN, HIGH);  // Disable SD
-
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), contaTick, RISING);
 
   Serial.begin(115200);
   while (!Serial) {}
-
 
   // SD card initialization
   if (!SD.begin(SD_CS_PIN)) {
     Serial.println(F("Errore inizializzazione SD!"));
     while (block);
-  }
-  else Serial.println(F("Scheda SD inizializzata con successo!"));
+  } else Serial.println(F("Scheda SD inizializzata con successo!"));
+
 
   // Open SD file for writing
-  digitalWrite(SD_CS_PIN, LOW);
   while (true) {
     sprintf(fileName, "%d.csv", fileNumber);
-
-    if (!SD.exists(fileName)) {
-      break;  // trovato numero libero
-    }
-
+    if (!SD.exists(fileName)) break;
     fileNumber++;
   }
 
   dataFile = SD.open(fileName, FILE_WRITE);
-  digitalWrite(SD_CS_PIN, HIGH);
   if (!dataFile) {
     Serial.println(F("Errore apertura file SD!"));
     while (block);
-  } 
-  else Serial.println(F("File aperto per scrittura."));
-  
+  } else Serial.println(F("File aperto per scrittura."));
 
 
   // RF24 radio initialization
-  digitalWrite(CSN_PIN, HIGH);
   if (!radio.begin()) {
     Serial.println(F("Radio hardware non risponde!"));
     while (block);
-  }
-  else Serial.println(F("Radio hardware pronto a trasmettere"));
+  } else Serial.println(F("Radio hardware pronto a trasmettere"));
+
 
   radio.setPALevel(RF24_PA_MAX);
   radio.setPayloadSize(sizeof(payload));
@@ -161,28 +132,16 @@ void setup() {
   radio.setAutoAck(false);
   radio.setDataRate(RF24_250KBPS);
 
-
-
-  //Gps setup
+  // GPS setup
   gpsSerial.begin(9600);
   Serial.println("In attesa del fix GPS...");
 
-
-  // // offset calibration for motor current
-  // for(int p = 0; p < 100; p++){               //first 100 values mean
-  //   sumCurrentMotor = sumCurrentMotor + analogRead(curr_motorPin);
-  //   delay(10);
-  // }
-  // CurrMotorOffset_voltage = sumCurrentMotor / 100;         //first 100 values mean
-  // sumCurrentMotor = 0;
-  // Serial.println(CurrMotorOffset_voltage);
-
-
   // Initial payload setup
-  payload.velocita = 1;
-  payload.voltage = 0;
-  payload.current = 0;
-  payload.currentMotor = 0;
+  payload.velocita_gps = 1;
+  payload.voltage_raw = 0;
+  payload.current_raw = 0;
+  payload.currentMotor_raw = 0;
+  payload.tick_count = 0;
   payload.lat = 45123456;
   payload.lng = 9123456;
   payload.micro = 0;
@@ -195,194 +154,157 @@ void setup() {
 void loop() {
   currentMicros = micros();
 
- // const unsigned long CALIBRATION_TIME = 5000000; // 5 secondi
-   //float sense_voltage_Mot = currentRaw / 1023.0 * 5.0;
-  //Moving Avarage Mot
-  // Aggiorna somma e buffer
- // totalMot -= samplesMot[indexMot]; // 1. Subtract the oldest sample from the total
-  //samplesMot[indexMot] = sense_voltage_Mot; // 2. Replace it with the new sample
-  //totalMot += sense_voltage_Mot; // 3. Add the new sample to the total
-  //indexMot = (indexMot + 1) % NUM_SAMPLES; // 4. Move the index to the next position (circular buffer)
-  //SenseAverageMot = totalMot / NUM_SAMPLES;// 5. Compute the average
-  
-  //if (currentMicros < CALIBRATION_TIME && not(CalibrationTimeEnded)) {
-    // Durante il warm-up, l'offset segue la media mobile
-    //CurrMotorOffset_voltage = SenseAverageMot; 
-  // }else{
-  //   CalibrationTimeEnded = 1;
-  // }
-
-  //currentMotor = (SenseAverageMot - CurrMotorOffset_voltage) / CurrMotorConst;
-  
-
-  
-  if(currentMicros - previousMicros >= delta) {
+  if (currentMicros - previousMicros >= delta) {
     previousMicros = currentMicros;
 
-    digitalWrite(SD_CS_PIN, HIGH);
-    digitalWrite(CSN_PIN, LOW);
+    payload.voltage_raw      = (int)round((float)sumVoltage_raw      / ncampioni);
+    payload.current_raw      = (int)round((float)sumCurrent_raw      / ncampioni);
+    payload.currentMotor_raw = (int)round((float)sumCurrentMotor_raw / ncampioni);
+    payload.tick_count       = tick;
+    payload.micro            = micros();
+    payload.verifica         = checksum();
 
-    if (currentMicros > (currentMicrov + tempoMaxFermo)) {
-      velocitaCalcolata = 0;
-      velocitaCalcolatam = 0;
-    }
+    radio.write(&payload, sizeof(payload));
+    tick = 0;
 
-    payload.voltage = sumVoltage / ncampioni;
-    payload.current = sumCurrent / ncampioni;
-    payload.currentMotor = motorCurrentRaw;      //valore di misura del sensore grezzo, senza nessuna conversione
-    payload.velocita = velocitaCalcolata;
-    payload.micro = micros();
-    payload.verifica = checksum();
-
-    
-    // Send data via NRF24
-    bool ACK = radio.write(&payload, sizeof(payload));
-
-    digitalWrite(CSN_PIN, HIGH);
-    digitalWrite(SD_CS_PIN, LOW);
-  
-
-    dtostrf(payload.voltage, 6, 2, vStr);
-    dtostrf(payload.current, 6, 2, cStr);
-    dtostrf(payload.currentMotor, 6, 2, cmStr);
-    dtostrf(payload.velocita, 6, 2, velStr);
-
-    sprintf(dataStr, "%ld, %s, %s, %s, %s, %ld, %ld\n",
+    sprintf(dataStr, "%ld, %d, %d, %d, %d, %d, %ld, %ld\n",
       payload.micro,
-      vStr,
-      cStr,
-      cmStr,
-      velStr,
+      payload.voltage_raw,
+      payload.current_raw,
+      payload.currentMotor_raw,
+      payload.tick_count,
+      payload.velocita_gps
       payload.lat,
       payload.lng
     );
 
-
     dataFile.print(dataStr);
 
-
-
-
-    if (i >= 10) {
+    if (i >= pacchetti_al_secondo) {
       stampa1();
-      Serial.println(F("10 pacchetti inviati"));
+      Serial.print(pacchetti_al_secondo);
+      Serial.println(" pacchetti inviati");
       i = 0;
       dataFile.flush();
     }
     i++;
 
     ncampioni = 0;
-    sumVoltage = 0;
-    sumCurrent = 0;
-    sumCurrentMotor = 0;
+    sumVoltage_raw = 0;
+    sumCurrent_raw = 0;
+    sumCurrentMotor_raw = 0;
   }
 
   ncampioni++;
-  sumVoltage = sumVoltage + ((analogRead(voltPin) - v_offset) * kv);
-  sumCurrent = sumCurrent + ((analogRead(currPin) - zeroCurr) * ki);
-  //sumCurrentMotor = sumCurrentMotor + (( analogRead(curr_motorPin) - CurrMotorOffset_voltage ) / CurrMotorConst);
-  // float motorCurrentRawFloat = analogRead(curr_motorPin);
-  //float motorCurrentRawFloat = motorCurrentRawInt/1023.0 *5.0;
-  motorCurrentRaw = analogRead(curr_motorPin);
+  sumVoltage_raw      += (analogRead(voltPin) * 10 - v_offset);
+  sumCurrent_raw      += (analogRead(currPin) * 10 - zeroCurr);
+  sumCurrentMotor_raw += (analogRead(curr_motorPin) * 10 - zeroCurrMotor);
 
 
   while (gpsSerial.available()) {
-      char c = gpsSerial.read();
-      //Serial.print(c);  // Stampa i dati grezzi per vedere cosa riceve
-      gps.encode(c);
+    char c = gpsSerial.read();
+    gps.encode(c);
 
     if (gps.location.isUpdated()) {
       payload.lat = gps.location.lat() * 1e6;
-      payload.lng = gps.location.lng() * 1e6;  
+      payload.lng = gps.location.lng() * 1e6;
+      payload.velocita_gps = (int)round(gps.speed.kmph() * 100);
+
+      if (!headerScritto) scriviHeader();
     }
   }
-  
 }
-
-
-
 
 int checksum() {
   int micro4 = payload.micro % 10000;
-  return payload.velocita + payload.voltage + payload.current + micro4;
+  return payload.tick_count + payload.voltage_raw + payload.current_raw + micro4;
 }
 
+// void calcolaVelocita() {
+//   currentMicrov = micros();
+//   deltatv = currentMicrov - lastMicrov;
+//   lastMicrov = currentMicrov;
+//   velocitaCalcolata = ((circonferenza / (float)n_fori) / (float)deltatv) * 1e6;
 
-void calcolaVelocita() {
-  //Serial.println("scatto");
-  currentMicrov = micros();
+//   j++;
+//   if (j == n_media) {
+//     j = 0;
+//     deltatvm = currentMicrov - lastMicrovm;
+//     lastMicrovm = currentMicrov;
+//     velocitaCalcolatam = (circonferenza / n_fori) / deltatvm * 1e6 * n_media;
+//   }
+// }
 
-  deltatv = currentMicrov - lastMicrov;
-  lastMicrov = currentMicrov; 
-  velocitaCalcolata = ((circonferenza / (float)n_fori) / (float)deltatv) * 1e6;
-  
-  
-  j++;
-  if(j==n_media)
-  {
-    j=0;
-    deltatvm = currentMicrov - lastMicrovm;
-    lastMicrovm = currentMicrov;
-    velocitaCalcolatam = (circonferenza / n_fori) / deltatvm * 1e6 * n_media;
-  }
+// void calcolaFrequenza() {
+//   currentMicrov = micros();
+//   deltatv = currentMicrov - lastMicrov;
+//   lastMicrov = currentMicrov;
+//   velocitaCalcolata = (1 / ((float)deltatv / 1e6));
+
+//   j++;
+//   if (j == n_media) {
+//     j = 0;
+//     deltatvm = currentMicrov - lastMicrovm;
+//     lastMicrovm = currentMicrov;
+//     velocitaCalcolatam = (1 / ((float)deltatvm / 1e6 / (float)n_media));
+//   }
+// }
+
+void contaTick(){
+  tick++;
 }
 
-void calcolaFrequenza() {         
-  //Serial.println("scatto");
-  currentMicrov = micros();
-
-  deltatv = currentMicrov - lastMicrov;
-  lastMicrov = currentMicrov; 
-  velocitaCalcolata = (1 / ((float)deltatv / 1e6));
-  
-  
-  j++;
-  if(j==n_media)
-  {
-    j=0;
-    deltatvm = currentMicrov - lastMicrovm;
-    lastMicrovm = currentMicrov;
-    velocitaCalcolatam = (1 / ((float)deltatvm / 1e6 / (float)n_media));
-
-  }
-}
-
-void tara_zeroCurr()
-{
+void tara_zeroCurr() {
   for (int i = 0; i < 3000; i++) {
-    zeroCurr = zeroCurr + analogRead(currPin);
+    zeroCurr += analogRead(currPin);
+    delay(1);
+    zeroCurrMotor += analogRead(curr_motorPin);
     delay(1);
   }
-  zeroCurr = zeroCurr / 3000;
-  Serial.println(zeroCurr);
+  zeroCurr = zeroCurr / 3000 * 10;
+  zeroCurrMotor = zeroCurrMotor / 3000 * 10;
 }
 
-void stampa1()
-{
+void scriviHeader() {
+  char header[40];
+  sprintf(header, "DATA: %04d-%02d-%02d %02d:%02d:%02d,0,0,0,0,0,0\n",
+    gps.date.year(),
+    gps.date.month(),
+    gps.date.day(),
+    gps.time.hour(),
+    gps.time.minute(),
+    gps.time.second()
+  );
+  dataFile.print(header);
+  headerScritto = true;
+}
+
+void stampa1() {
   Serial.println(F("=== Pacchetto inviato ==="));
-  Serial.print(F("raw verifica:   ")); Serial.println(payload.verifica);
-  Serial.print(F("velocita:       ")); Serial.println(payload.velocita, 6);
-  Serial.print(F("voltage:        ")); Serial.println(payload.voltage);
-  Serial.print(F("current:        ")); Serial.println(payload.current);
-  Serial.print(F("motorCurrent:        ")); Serial.println(payload.currentMotor);
+  Serial.print(F("velocita GPS:   ")); Serial.println(payload.velocita_gps, 6);
+  Serial.print(F("voltage:        ")); Serial.println(payload.voltage_raw);
+  Serial.print(F("current:        ")); Serial.println(payload.current_raw);
+  Serial.print(F("motorCurrent:   ")); Serial.println(payload.currentMotor_raw);
+  Serial.print(F("tick_count:   ")); Serial.println(payload.tick_count);
   Serial.print(F("lat (raw):      ")); Serial.println(payload.lat);
   Serial.print(F("lng (raw):      ")); Serial.println(payload.lng);
   Serial.print(F("micro:          ")); Serial.println(payload.micro);
+  Serial.print(F("verifica:          ")); Serial.println(payload.verifica);
+  Serial.print(F("zerocurrmotor:          ")); Serial.println(zeroCurrMotor);
 }
 
-void stampa2()
-{
+void stampa2() {
   Serial.println(F("=== valori letti ==="));
-  Serial.print(F("tensione raw:   ")); Serial.println(analogRead(voltPin));
-  Serial.print(F("corrente raw:   ")); Serial.println(analogRead(currPin));
-  Serial.print(F("correntemotore raw:   ")); Serial.println(analogRead(curr_motorPin));
-  Serial.print(F("voltage:        ")); Serial.println(payload.voltage);
-  Serial.print(F("current:        ")); Serial.println(payload.current);
+  Serial.print(F("tensione raw:       ")); Serial.println(analogRead(voltPin));
+  Serial.print(F("corrente raw:       ")); Serial.println(analogRead(currPin));
+  Serial.print(F("correntemotore raw: ")); Serial.println(analogRead(curr_motorPin));
+  Serial.print(F("voltage:            ")); Serial.println(payload.voltage_raw);
+  Serial.print(F("current:            ")); Serial.println(payload.current_raw);
 }
 
-void stampa3()
-{
-  Serial.println(F("=== valori velocita ==="));
-  Serial.print("Velocità: "); Serial.println(velocitaCalcolata);
-  Serial.print(" - Media: "); Serial.println(velocitaCalcolatam);
-}
+// void stampa3() {
+//   Serial.println(F("=== valori velocita ==="));
+//   Serial.print(F("Velocità: ")); Serial.println(velocitaCalcolata);
+//   Serial.print(F(" - Media: ")); Serial.println(velocitaCalcolatam);
+//   Serial.print(F(" - GPS:   ")); Serial.println(payload.velocita_gps);
+// }
