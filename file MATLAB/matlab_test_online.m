4d2d837f27eeeac9c0bf94b9e6b965fd60d8ca52
federico23-poli <federico.lai@mail.polimi.it>
2026-05-06 11:41:46 +0200

    Update matlab_test_online.m

--- file MATLAB/matlab_test_online.m
+++ file MATLAB/matlab_test_online.m
@@ -5,73 +5,75 @@ clear
 %=======================================================COSTANTI
 n_pacchetti = 10;   %pacchetti al secondo
 timeplotted = 20;    %ultimi tot secondi che si vogliono visualizzare
-delta = 1;    %tempo di aggiornamento in secondi
+delta = 2;    %tempo di aggiornamento in secondi
 
-voltage = 2;  % n° delle COLONNE delle grandezze nel file excel
+voltage = 2;  % n° della COLONNA delle grandezze nel file excel
 battCurr = 3;
 motorCurr = 4;
 speed = 5;
 lat = 6;
 lon = 7;
 
-FILE = "simulazione1.csv";
+FILE = "serial_data.csv";
 
-%==================================================INIZIO DEL CICLO
+%==================================================VISUALIZZAZIONE DELLA FINESTRA
 cputime_prev = cputime;
 pause(delta)
+figure('WindowState','maximized')
+t = tiledlayout(5,1,'Padding','compact','TileSpacing','compact');
+ax1 = nexttile;
+ax2 = nexttile;
+ax3 = nexttile;
+ax4 = nexttile;
+ax5 = nexttile;
+zoom on
+pan on
+
+%==================================================INIZIO DEL CICLO
 while true
     while cputime - cputime_prev < delta         %aspetta delta secondi prima di plottare di nuovo
     end
-    cputime_prev = cputime;
-    if true  
-        Data=readmatrix(FILE);  
-        tot_timebase = Data(:,1)/1000000;
-        endbuffer = length(tot_timebase);
-        startbuffer = endbuffer - (n_pacchetti * timeplotted); 
-        if startbuffer < 1
-            startbuffer = 1;
-        end
-        timebase = tot_timebase(startbuffer:endbuffer)/1000000;
-        databuffer = Data(startbuffer:endbuffer, :);
+    cputime_prev = cputime;  
+    Data=readmatrix(FILE);  
+    tot_timebase = Data(:,1)/1000000;
+    endbuffer = length(tot_timebase);
+    startbuffer = endbuffer - (n_pacchetti * timeplotted); 
+    if startbuffer < 1
+        startbuffer = 1;
     end
-    primo_ciclo = 0;
+    timebase = tot_timebase(startbuffer:endbuffer)/1000000;
+    databuffer = Data(startbuffer:endbuffer, :);
 
     
     %BATTERY VOLTAGE
-    figure(1)
-    subplot(5,1,1)
-    plot(timebase,databuffer(:,voltage),'.')
+    plot(ax1, timebase,databuffer(:,voltage),'.')
     xlabel('time s')
-    ylabel('tensione batteria[V]')
+    ylabel(ax1, 'tensione batteria[V]')
     grid on 
     
     %BATTERY CURRENT
-    subplot(5,1,2)
-    plot(timebase,databuffer(:,battCurr),'.')
+    plot(ax2, timebase,databuffer(:,battCurr),'.')
     xlabel('time s')
-    ylabel('corrente batteria [A]')
+    ylabel(ax2, 'corrente batteria [A]')
     grid on 
     
     %MOTOR CURRENT
-    subplot(5,1,3)
-    plot(timebase,databuffer(:,motorCurr),'.')
+    plot(ax3, timebase,databuffer(:,motorCurr),'.')
     xlabel('time s')
-    ylabel('corrente motore [A]')
+    ylabel(ax3, 'corrente motore [A]')
+    grid on
     
     %SPEED
-    grid on 
-    subplot(5,1,4)
-    plot(timebase,databuffer(:,speed),'.')
+    plot(ax4, timebase,databuffer(:,speed),'.')
     xlabel('time s')
-    ylabel('speed [km/h]')
+    ylabel(ax4, 'speed [km/h]')
      ylim([0,35]);
      grid on 
     
     %GPS
-    subplot(5,1,5)
-    plot(databuffer(:,lat),databuffer(:,lon),'.')
-    xlabel('latitudine')
-    ylabel('longitudine')
+    plot(ax5, databuffer(:,lat),databuffer(:,lon),'.')
+    xlabel(ax5, 'latitudine')
+    ylabel(ax5, 'longitudine')
     
     %GPS SPEED
     %lat_metri =
@@ -79,6 +81,9 @@ while true
     %v_lat = gradient(lat_metri, timebase);
     %v_lon = gradient(lon_metri, timebase);
     %GPSspeed = sqrt(v_lat^2 + v_lon^2);
+
+    drawnow
+    pause(0.1)
 end
 
 
@@ -98,5 +103,6 @@ i_start = find(abs(timebase - t_start) < 1,  1);         %cerca l'indice di t_st
 i_end = find(abs(timebase - t_end) < 1,  1);
 W = Data(i_start:i_end, 2).*Data(i_start:i_end, 3);  % Watt = V * A
 E = trapz(timebase(i_start:i_end), W)   /  3.6*1e-6;    %conversione Joule-kWh;  SCEGLIERE VALORE FINALE E INIZIALE DI INTEGRAZIONE
+
 
 
